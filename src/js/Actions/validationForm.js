b41da9fc6f11331a0977f9f58614bcad2726de73
VovaSidorov <VovaCivic@gmail.com>
2019-12-23 17:41:21 +0200

    Proba ActionEvents

---
+++ src/js/Actions/validationForm.js
@@ -0,0 +1,37 @@
+export default (event) => {
+console.log("sdfsdfsdfsdfsdf");
+
+let regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+regPass = /^([A-Za-z0-9_\-\.]){6,}/,
+email = document.getElementById("email").value,
+pass=document.getElementById("password").value,
+incorrectEmail = document.getElementById("incorrectEmail"),
+incorrectPass = document.getElementById("incorrectPass");
+
+
+
+console.log(regEmail.test(email));
+console.log(regPass.test(pass));
+
+if (regPass.test(pass) == true && regEmail.test(email) == true){
+  checkingItemsInDataBase();
+}
+if(regEmail.test(email) == false){
+  incorrectEmail.style.display = "block";
+}
+if(regPass.test(pass) == false){
+  incorrectPass.style.display = "block";
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,21 @@ import render from './lib/renderHtml';
 import header from './Components/Header/';
 import loginPage from './Components/LoginPage';
 
+import validationForm from "./actions/validationForm";
 
-render("app", header);
-render("app", loginPage);
+
+localStorage.getItem(name)==null ? render("app", loginPage) : render("app", header);
+
+var elem = document.getElementById("btn");
+
+elem.addEventListener('click', function(event){
+    event.preventDefault();
+    let incorrectEmail = document.getElementById("incorrectEmail"),
+    incorrectPass = document.getElementById("incorrectPass");
+incorrectEmail.style.display = "";
+incorrectPass.style.display = "";
+    validationForm();
+});
 
 
 
